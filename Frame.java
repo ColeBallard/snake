915ca5315bd8bab1ab0cf1881bfa7c22c6e6bc77
Cole Ballard <cole.ballard97@gmail.com>
2022-09-23 13:08:57 -0500

    add basic frame

---
+++ Frame.java
@@ -0,0 +1,15 @@
+import javax.swing.JFrame;
+
+public class Frame extends JFrame {
+
+    Frame() {
+        this.add(new Panel());
+        this.setTitle("Snake");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.pack();
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
+    }
+
+}
---
+++ Panel.java
@@ -0,0 +1,14 @@
+import javax.swing.JPanel;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Panel extends JPanel implements ActionListener {
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // TODO Auto-generated method stub
+        
+    }
+    
+}
